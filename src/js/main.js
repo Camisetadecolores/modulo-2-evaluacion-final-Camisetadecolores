ce20d403e577e8a1f56e43cf65a9ffa8ef6d6ac9
Camisetadecolores <camisetadecolores@MacBook-Air-de-Cristina.local>
2025-12-16 00:16:12 +0100

    firsts steps

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+
+// CONSTANTS + DOMS
+
+const form = document.querySelector('#form');
+const inputUser = document.querySelector('#inputUser');
+const buttonSearch = document.querySelector('#buttonSearch');
+
+//FUNCTIONS
+
+
+
+
